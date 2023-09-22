e4baeb1a9846ccb4e8d3b34d151d008f32b84d51
verbsrg <h4rdw4y@gmail.com>
2023-09-22 23:51:07 +0200

    fixed styles

--- app/_components/CurrencyConverter/CurrencyConverter.tsx
+++ app/_components/CurrencyConverter/CurrencyConverter.tsx
@@ -46,8 +46,7 @@ const CurrencyConverter: React.FC<ExchangeModel> = ({
         <input
           value={convertedAmount?.toFixed(2)}
           type="number"
-          className="mb-6 bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-l-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 cursor-not-allowed dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          disabled
+          className="mb-6 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-l-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 cursor-not-allowed dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         ></input>
         <select
           value={toCurrency}
@@ -59,7 +58,7 @@ const CurrencyConverter: React.FC<ExchangeModel> = ({
           ))}
         </select>
       </div>
-      <div className="mt-5">
+      <div className="mt-5 text-xs sm:text-sm text-slate-300">
         Rates last updated: <span>{date.toUTCString()}</span>
       </div>
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default async function Home() {
   return (
     <div className="bg-gradient-radial from-blue-500 via-blue-900">
       <div className="flex flex-col items-center justify-center h-screen">
-        <h1 className="font-extrabold text-[64px] text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-white">
+        <h1 className="font-extrabold text-[32px] sm:text-[64px] text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-white">
           Currency converter
         </h1>
         <CurrencyConverter {...data} />
